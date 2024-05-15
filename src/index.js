22a010dacd0bc607a0b863add6de9cdc20918981
Denise148 <167827528+Denise148@users.noreply.github.com>
2024-05-14 18:29:27 -0600

    Api search

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
+function updateWeatherUnit(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
+}
+
+function searchCity(city) {
+  let apiKey = "20b266eb57t5a497fa9e0073d3a3017o";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
+  console.log(apiUrl);
+  axios.get(apiUrl).then(updateWeatherUnit);
+}
+
 function searchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", searchSubmit);
+searchCity("Albuquerque");
