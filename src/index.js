ecca7a1e7c0e000ef6c358d3dc76e62d64b5826a
Denise148 <167827528+Denise148@users.noreply.github.com>
2024-05-16 18:49:52 -0600

    Added forecast api/ css

--- src/index.js
+++ src/index.js
@@ -58,20 +58,29 @@ function getForecast(city) {
   let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=imperial`;
   axios.get(apiUrl).then(displayForecast);
 }
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  return days[date.getDay()];
+}
 function displayForecast(response) {
-  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
   let forecastHtml = "";
-  days.forEach(function (day) {
-    forecastHtml =
-      forecastHtml +
-      `
-            <div class="forecast-day">${day}</div>
-            <div class="forecast-icon">☀️</div>
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
+      forecastHtml =
+        forecastHtml +
+        `
+            <div class="forecast-day">${formatDay(day.time)}</div>
+            <div class="forecast-icon">
+            <img src="${day.condition.icon_url}"class="forecast-icon"></div>
             <div class="forecast-temperature">
-              <div class="temps"><strong>18°</strong> </div>
-              <div class="temps">12°</div>
+              <div class="temps"><strong>${Math.round(
+                day.temperature.maximum
+              )}°</strong> </div>
+              <div class="temps">${Math.round(day.temperature.minimum)}°</div>
             </div>
           </div>`;
+    }
   });
   let forecastElement = document.querySelector("#forecast");
   forecastElement.innerHTML = forecastHtml;
